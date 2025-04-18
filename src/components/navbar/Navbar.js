f20238e97147fd5bc0b15bfb5985eb3123e3f3da
Annabel <annabel_wulf@estudante.sesisenai.org.br>
2025-04-18 19:14:16 -0300

    menu options

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,37 +5,38 @@ import "./Navbar.css";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-// react icons
-import { BiCart } from "react-icons/bi";
-import { BiSolidCart } from "react-icons/bi";
-import { RiSwordLine } from "react-icons/ri";
-import { RiSwordFill } from "react-icons/ri";
-import { TbCards } from "react-icons/tb";
-import { TbCardsFilled } from "react-icons/tb";
-
 function Navbar() {
+  
   const navigate = useNavigate();
   const [selectedIcon, setSelectedIcon] = useState("");
 
   const icons = [
-    { path: "/store", name: "Loja", icon: BiCart, selectedIcon: BiSolidCart },
     {
       path: "/",
-      name: "Batalha",
-      icon: RiSwordLine,
-      selectedIcon: RiSwordFill,
+      name: "BATALHA",
+      icon: "/assets/icons/battle.png",
+      color: "#4C68F7",
+      selectedColor: "#131D53",
     },
     {
-      name: "/collection",
-      name: "Coleção",
-      icon: TbCards,
-      selectedIcon: TbCardsFilled,
+      path: "/collection",
+      name: "COLEÇÃO",
+      icon: "/assets/icons/collection.png",
+      color: "#EC5252",
+      selectedColor: "#9A3535",
     },
+    {
+      path: "/store",
+      name: "LOJA",
+      icon: "/assets/icons/store.png",
+      color: "#FBC213",
+      selectedColor: "#B58B0A",
+    }
   ];
 
-  const changePage = (page) => {
-    setSelectedIcon(page);
-    navigate(page);
+  const changePage = (option) => {
+    setSelectedIcon(option.name);
+    navigate(option.path);
   };
 
   return (
@@ -44,35 +45,48 @@ function Navbar() {
         <div className="navbar_profile_box">
           <div className="navbar_profile_icon" />
           <div className="navbar_profile_info_box">
-            <img src="/assets/icons/level.png" alt="Level" width={"25px"} height={"25px"} />
+            <img
+              src="/assets/icons/level.png"
+              alt="Level"
+              width={"25px"}
+              height={"25px"}
+            />
             <div className="navbar_profile_info">40/560</div>
           </div>
           <div className="navbar_profile_info_box">
-            <img src="/assets/icons/coin.png" alt="Moeda" width={"25px"} height={"25px"} />
+            <img
+              src="/assets/icons/coin.png"
+              alt="Moeda"
+              width={"25px"}
+              height={"25px"}
+            />
             <div className="navbar_profile_info">6500</div>
           </div>
         </div>
         <div className="navbar_options_box">
-          {icons.map((option, index) =>
-            selectedIcon === option.name ? (
-              <div className="navbar_option">
-                <option.selectedIcon
-                  key={index}
-                  className="navbar_icon selected"
-                />
-                <p>{option.name}</p>
-              </div>
-            ) : (
-              <div className="navbar_option">
-                <option.icon
-                  className="navbar_icon unselected"
-                  key={index}
-                  onClick={() => changePage(option.name)}
-                />
-                <p>{option.name}</p>
-              </div>
-            )
-          )}
+          {icons.map((option, index) => (
+            <div
+              className="navbar_option"
+              style={{
+                backgroundColor: option.color,
+                width: selectedIcon === option.name ? "45%" : "40%",
+                borderRight: selectedIcon === option.name ? `20px solid ${option.selectedColor}` : "none",
+              }}
+              onClick={() => changePage(option)}
+              key={index}
+            >
+              <p>{option.name}</p>
+              <img
+                src={option.icon}
+                className={`navbar_icon ${
+                  selectedIcon === option.name ? "selected" : "unselected"
+                }`}
+                alt={option.name}
+                width="25px"
+                height="25px"
+              />
+            </div>
+          ))}
         </div>
         <img className="navbar_logo" src="/logo.png" alt="Minha Imagem" />
       </div>
